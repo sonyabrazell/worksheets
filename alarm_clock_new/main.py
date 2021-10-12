82331add01dbf1d9618fe91213de4654a0f18064
Sonya Brazell <sonyabrazell@Sonyas-iMac.local>
2021-10-12 12:05:36 -0500

    Updated main page with correct code.

--- alarm_clock_new/main.py
+++ alarm_clock_new/main.py
@@ -2,7 +2,7 @@ from alarm_clock import AlarmClock
 
 alarm = AlarmClock()
 
-AlarmClock.change_alarm(AlarmClock)
-AlarmClock.alarm_time(AlarmClock)
-AlarmClock.alarm_set(AlarmClock)
+alarm.change_alarm()
+alarm.alarm_time()
+alarm.alarm_set()
 
